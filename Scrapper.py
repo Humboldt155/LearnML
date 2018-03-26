06684b7c7e0904238a39a18e11c8329981490129
Humboldt155 <petr_kozyrev@icloud.com>
2018-03-26 23:51:03 +0300

    3

--- Scrapper.py
+++ Scrapper.py
@@ -1,22 +1,28 @@
-
 import requests
 from bs4 import BeautifulSoup
-from lxml import html
+import re
 
-url = 'https://leroymerlin.ru/product/kraska-vodno-dispersionnaya-cvet-belyy-2-5-kg-17664292/'
+url = 'https://leroymerlin.ru/product/radiator-monlan-500-70-15052760/'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
 
-# headers = {
-#         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'
-#       }
-
-page = requests.get(url)
+page = requests.get(url, headers=headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
+attributes = soup.find_all('table', {'class': 'about__params__table'})
 
-attributes = soup.find('table', {'class': 'about__params__table'})
+rows = attributes[0].find_all('td')
 
-print(attributes)
+for i in range(0, len(rows)):
+    rows[i] = str(rows[i]).replace("  ", "")
+    rows[i] = str(rows[i]).replace("<td>", "")
+    rows[i] = str(rows[i]).replace("</td>", "")
+    rows[i] = str(rows[i]).replace("\n", "")
+    rows[i] = re.sub('<[^>]+>', ' ', rows[i])
+    rows[i] = str(rows[i]).replace(" ? ", "")
 
+oneDict = {}
+for i in range(0, len(rows), 2):
+    oneDict[rows[i]] = rows[i + 1]
 
-# with open('test.html', 'w') as output_file:
-#     output_file.write(page.text.encode('cp1251'))
+for key in oneDict:
+    print(key + " " + oneDict[key])
