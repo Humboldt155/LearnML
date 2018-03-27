59083cc3973037fcb86909e0260f49d65dcbedcc
Humboldt155 <petr_kozyrev@icloud.com>
2018-03-27 23:47:41 +0300

    4

--- Scrapper.py
+++ Scrapper.py
@@ -5,33 +5,71 @@ import re
 
 import pandas as pd
 
-urls = pd.read_csv('urls.csv', header=None)
+urls = pd.read_csv(open('URL Адреса для теста.csv', 'r'), delimiter=';')
+
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0'}
 
+#
+
+listAll = []
+
+ind = 0
 
 for row in urls.itertuples():
+    ind += 1
+    if ind > 20: break
     dep = str(row[1])
-    url = str(row[2])
+    model = str(row[2])
+    code = str(row[3])
+    url_lm = row[4]
+    url_bitrix_step = row[5]
 
-    page = requests.get(url, headers=headers, auth=HTTPBasicAuth('greensight', 'greensight'), verify=False)
+    page_lm = requests.get(url_lm, headers=headers, verify=False)
+    page_bitrix_step = requests.get(url_bitrix_step, headers=headers, auth=HTTPBasicAuth('greensight', 'greensight'),
+                                    verify=False)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-    attributes = soup.find_all('table', {'class': 'about__params__table'})
+    soup_lm = BeautifulSoup(page_lm.content, 'html.parser')
+    soup_bitrix_step = BeautifulSoup(page_bitrix_step.content, 'html.parser')
 
-    rows = attributes[0].find_all('td')
+    attributes_lm = soup_lm.find_all('table', {'class': 'about__params__table'})
+    attributes_bitrix_step = soup_bitrix_step.find_all('table', {'class': 'about__params__table'})
 
-    for i in range(0, len(rows)):
-        rows[i] = str(rows[i]).replace("<td>", "")
-        rows[i] = str(rows[i]).replace("</td>", "")
-        rows[i] = str(rows[i]).replace("\n", "")
-        rows[i] = str(rows[i]).replace("  ", "")
-        rows[i] = re.sub('<[^>]+>', ' ', rows[i])
-        rows[i] = str(rows[i]).replace(" ? ", "")
+    rows_lm = attributes_lm[0].find_all('td')
+    rows_bitrix_step = attributes_bitrix_step[0].find_all('td')
 
-    oneDict = {}
-    for i in range(0, len(rows), 2):
-        oneDict[rows[i]] = rows[i + 1]
+    maxLines = max(len(rows_lm), len(rows_bitrix_step))
 
-    for key in oneDict:
-        print(dep + " " + key + " " + oneDict[key])
+    attrDict = {}
 
+    for i in range(0, len(rows_lm)):
+        rows_lm[i] = str(rows_lm[i]).replace("<td>", "")
+        rows_lm[i] = str(rows_lm[i]).replace("</td>", "")
+        rows_lm[i] = str(rows_lm[i]).replace("\n", "")
+        rows_lm[i] = str(rows_lm[i]).replace("  ", "")
+        rows_lm[i] = re.sub('<[^>]+>', ' ', rows_lm[i])
+        rows_lm[i] = str(rows_lm[i]).replace(" ? ", "")
+
+    for i in range(0, len(rows_bitrix_step)):
+        rows_bitrix_step[i] = str(rows_bitrix_step[i]).replace("<td>", "")
+        rows_bitrix_step[i] = str(rows_bitrix_step[i]).replace("</td>", "")
+        rows_bitrix_step[i] = str(rows_bitrix_step[i]).replace("\n", "")
+        rows_bitrix_step[i] = str(rows_bitrix_step[i]).replace("  ", "")
+        rows_bitrix_step[i] = re.sub('<[^>]+>', ' ', rows_bitrix_step[i])
+        rows_bitrix_step[i] = str(rows_bitrix_step[i]).replace(" ? ", "")
+
+    for i in range(0, len(rows_lm), 2):
+        attrDict[rows_lm[i]] = [rows_lm[i], rows_lm[i + 1], '', '']
+
+    for i in range(0, len(rows_bitrix_step), 2):
+        if rows_bitrix_step[i] in attrDict:
+            attrDict[rows_bitrix_step[i]][2] = rows_bitrix_step[i]
+            attrDict[rows_bitrix_step[i]][3] = rows_bitrix_step[i+1]
+        else:
+            attrDict[rows_bitrix_step[i]] = ['', '', rows_bitrix_step[i], rows_bitrix_step[i+1]]
+
+
+    for attr in attrDict:
+        listAll.append([dep, model, code, attrDict[attr][0], attrDict[attr][1], attrDict[attr][2], attrDict[attr][3]])
+
+for row in listAll:
+    print(row)
